affdccc1b03153c623542f6287571e58866edf5f
Ishav bhatt <ishavbhattkhaniyara.com>
2019-08-30 22:18:30 +0530

    chrome extension

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,1 +1,23 @@
-// You js goes here
+// Your js goes here
+var lists = [ { thought: "ctrl a",
+				author: "it  is ctrl a" },
+
+			 { thought: "ctrl b",
+			 	author: "it is ctrl b" },
+
+			 { thought: "ctrl c",
+			 	author: "it is ctrl c" }
+]
+
+function send() {
+var randomNumber = Math.floor(Math.random() * lists.length);
+
+var thought = document.getElementsByClassName("heading");
+thought.innerText = lists[randomNumber].thought;
+
+var author = document.getElementsByClassName("paragraph");
+author.innerText = lists[randomNumber].author;
+
+}
+document.getElementById("button1").addEventListener("click", send);
+
